154cbf8e7030ec62e6fbe9a44f2031a1898ad3e1
Peter Cai <peter@typeblog.net>
2016-05-02 19:46:34 +0800

    Initial Commit

---
+++ zn.sh
@@ -0,0 +1,33 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+if [[ -z "$ZERONET_HOME" ]]; then
+	echo "--- Installing ZeroNet ---"
+	apt-get -y update && apt-get -y upgrade
+	apt-get install -y curl make python2-dev git gcc grep c-ares-dev libev-dev
+	export LIBEV_EMBED=false
+	export CARES_EMBED=false
+	pip2 install gevent msgpack-python
+	termux-setup-storage
+
+	if [[ ! -d ~/storage/shared/ZeroNet ]]; then
+		mkdir ~/storage/shared/ZeroNet
+		git clone https://github.com/HelloZeroNet/ZeroNet.git ~/storage/shared/ZeroNet
+	fi
+
+	cp "$0" ~/zn.sh
+	echo 'export ZERONET_HOME=~/storage/shared/ZeroNet' >> ~/.bashrc
+	echo "alias zn=\"bash ~/zn.sh\"" >> ~/.bashrc
+	source ~/.bashrc
+
+fi
+
+if [[ "$1" == "update" ]]; then
+	echo "--- Updating ZeroNet ---"
+	pushd $ZERONET_HOME
+	git pull
+	popd
+else
+	pushd $ZERONET_HOME
+	python2 zeronet.py $@
+	popd
+fi
